7b42399776e4faf3f9d9f84bed28c1145e6861eb
Santiago <santiagosaav.99@gmail.com>
2020-11-12 14:41:33 -0300

    Corrección al tamaño de los libros
    Se corrigió el tamaño de los libros, además, se convirtió el App.js en una clase extendida de componentes

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,3 +1,4 @@
+import React, {Component} from 'react';
 import './App.css';
 import Header from '../components/Header/Header';
 import Navbar from '../components/Navbar/Navbar';
@@ -5,44 +6,53 @@ import Sidebar from '../components/Sidebar/Sidebar';
 import Book from '../components/Book/Book';
 import imageExample from '../assets/Body/book-example.jpg'; //BORRAR CUANDO SE IMPLEMENTE LA DB
 
-function App() {
-  return (
-    <div className="App">
-      <div className="App-header">
-        <Header/>
-        <Navbar
-          novedades={'#'} 
-          autores={'#'}
-          categorias={'#'}
-          carrito={'#'}
-        />
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <div className="App-header">
+          <Header/>
+          <Navbar
+            novedades={'#'} 
+            autores={'#'}
+            categorias={'#'}
+            carrito={'#'}
+          />
+        </div>
+        <div className="App-body">
+          <Sidebar id="sidebar"/>
+          <div className="body"> 
+            <Book //Se han agregado 3 libros a modo de ejemplo
+            image={imageExample} 
+            nombre={'Los pies del silencio'}
+            autor={'María José Terré'}
+            precio={'13.000'}
+            />
+            <Book 
+            image={imageExample}
+            nombre={'Los pies del silencio'}
+            autor={'María José Terré'}
+            precio={'13.000'}
+            />
+            <Book 
+            image={imageExample}
+            nombre={'Los pies del silencio'}
+            autor={'María José Terré'}
+            precio={'13.000'}
+            />
+            <Book 
+            image={imageExample}
+            nombre={'Los pies del silencio'}
+            autor={'María José Terré'}
+            precio={'13.000'}
+            />
+          </div> 
+        </div>
+        <br/>
       </div>
-      <div className="App-body">
-        <Sidebar id="sidebar"/>
-        <div className="body"> 
-          <Book //Se han agregado 3 libros a modo de ejemplo
-          image={imageExample} 
-          nombre={'Los pies del silencio'}
-          autor={'María José Terré'}
-          precio={'13.000'}
-          />
-          <Book 
-          image={imageExample}
-          nombre={'Los pies del silencio'}
-          autor={'María José Terré'}
-          precio={'13.000'}
-          />
-          <Book 
-          image={imageExample}
-          nombre={'Los pies del silencio'}
-          autor={'María José Terré'}
-          precio={'13.000'}
-          />
-        </div> 
-      </div>
-      <br/>
-    </div>
-  );
+    );
+  }
 }
+  
 
 export default App;
